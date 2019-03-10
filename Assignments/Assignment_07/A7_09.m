684fc79208d7e5c7482a83a216ce9ea622c2f5c0
Lucas <lucasrm25@gmail.com>
2019-03-10 01:07:11 +0100

    Uploading Assingment 07

---
+++ Assignments/Assignment_07/A7_09.m
@@ -0,0 +1,22 @@
+close all;clear;clc;
+%% You can use any command in MPT in this assignment
+
+%% Question 1
+% write the code to plot X0. In the report, give Pf,  the X0, and your
+% motivation for choosing this Pf.
+
+%% Question 2
+% write the code to plot the requested figures. Provide the figures in your
+% report and explain your observation about the error between the state and
+% state prediction as N increases.
+
+%% Question 3
+% no code is needed. Answer in the report
+
+%% Question 4
+% write a code that calculates the figures and costs. Provide the figures
+% and costs in the report. for costs, provide a table in the report that
+% provides all costs for all different methods in the question (4 methods,
+% each with three different costs as defined in A7 assignment). If you what
+% to use some functions in the code, you can write them in different matlab
+% files and submit them with the rest of your files
